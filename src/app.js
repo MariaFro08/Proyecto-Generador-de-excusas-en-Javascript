cdff4b5073e0d996673a2a03f9bd81e1a6fdddfc
MariaFro08 <maria.rodriguez.marino@anda.com.uy>
2024-08-02 23:59:29 +0000

    Proyecto javascript terminado y entregado, no me aceptó los style.css

--- src/app.js
+++ src/app.js
@@ -1,11 +1,46 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+function GenerarExcusa() {
+  let Quien = [
+    "Mi perro",
+    "El vecino",
+    "Un extraterreste",
+    "Mi periquito",
+    "Mi tatarabuela",
+    "El Ratón Pérez",
+    "La tortuga de mi prima"
+  ];
+  let Accion = [
+    "se comió",
+    "rompió",
+    "perdió",
+    "escondió",
+    "regaló",
+    "me robó",
+    "congeló en el freezer"
+  ];
+  let Que = [
+    "mi tarea",
+    "el coche",
+    "la computadora",
+    "mis llaves",
+    "mi celular",
+    "un libro que estaba debajo de la cama",
+    "los anteojos de mi mamá"
+  ];
+  let Cuando = [
+    "antes de la clase",
+    "cuando estaba bailando",
+    "mientras hacia ejercicio",
+    "durante mi almuerzo",
+    "mientras estaba orando",
+    "cuando fui a trabajar",
+    "mientras estaba de viaje por Suiza"
+  ];
+  let QuienAleatorio = Math.floor(Math.random() * Quien.length);
+  let AccionAleatorio = Math.floor(Math.random() * Accion.length);
+  let QueAleatorio = Math.floor(Math.random() * Que.length);
+  let CuandoAleatorio = Math.floor(Math.random() * Cuando.length);
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let excuse = `${Quien[QuienAleatorio]} ${Accion[AccionAleatorio]} ${Que[QueAleatorio]} ${Cuando[CuandoAleatorio]}.`;
+  document.getElementById("excuse").innerHTML = excuse;
+}
+window.onload = GenerarExcusa;
